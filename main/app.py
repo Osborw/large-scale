6af0bb6b9a84fd5a53076b4faf14eb0248f52108
Wayne Osborn <osborw@rpi.edu>
2018-11-05 22:08:52 -0500

    Removing json import

--- main/app.py
+++ main/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-from json import dumps
 import data
 
 app = Flask(__name__)
--- main/crawling.py
+++ main/crawling.py
@@ -1,6 +1,5 @@
 from flask import jsonify, request
 from flask_restful import Resource, Api
-from json import dumps
 
 class Crawling(Resource):
 
